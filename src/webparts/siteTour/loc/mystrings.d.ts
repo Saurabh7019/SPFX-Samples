2b1c8ed3a36cb98a055db79da343b6a5b8d80482
Saurabh <saurabh.tripathi@epicalgroup.com>
2024-05-29 10:30:05 +0200

    Initial commit

---
+++ src/webparts/siteTour/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISiteTourWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SiteTourWebPartStrings' {
+  const strings: ISiteTourWebPartStrings;
+  export = strings;
+}
